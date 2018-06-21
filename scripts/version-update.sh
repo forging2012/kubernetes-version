7e80fef4f3a41e8edca7c3565d4a290662753f2b
Alan Peng <peng.alan@gmail.com>
2018-06-21 15:13:41 +0800

    Update version-update.sh

--- scripts/version-update.sh
+++ scripts/version-update.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+cd /data/alanpeng/kubernetes-versions/scripts/
 ./get-k8s-version-list.sh
 diff ./k8s-version-new.txt ./k8s-version.txt |grep v |awk '{print $2}' > ./k8s-version-diff.txt
 
 if test -s ./k8s-version-diff.txt; then
-  cat ./k8s-version-diff.txt |mail -s 'New kubernetes release!' alan_peng@wise2c.com
   ./uploadfiles.sh
-else
-  echo 'No new kubernetes release found.' |mail -s 'No new kubernetes release found.' alan_peng@wise2c.com
+  cat ./k8s-version-diff.txt |mail -s 'New kubernetes release!' alan_peng@wise2c.com
 fi
